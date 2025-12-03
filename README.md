ad0587656e1b96e96b6b7f4977407e19c79763ba
martinsanihan <ihancamilo.sm@gmail.com>
2025-12-03 20:28:55 -0300

    avance 3 de diciembre

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tarea 3 Sistemas Operativos: Paginación
--- tarea3.c
+++ tarea3.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <math.h>
 
 #define RANGO_A 512
 #define RANGO_B 1024
@@ -12,25 +17,48 @@ int main() {
     float randomCeroUno = (float)rand() /RAND_MAX;
     float random = (a + randomCeroUno * (b - a));
 
-    printf("Cuánta Memoria Física (RAM) Tienes?\n");
+    printf("Ingresar cantidad de memoria física (RAM) en MB:\n");
     scanf("%f", &memFisica);
     int intMemFisica = (int)memFisica;
     float memVirtual = (random * memFisica);
+    int intMemVirtual = (int)memVirtual;
 
     float pageSize;
-    printf("Tamaño de Páginas?\n");
+    printf("Ingresar tamaño de cada página:\n");
     scanf("%f", &pageSize);
     int intPageSize = (int)pageSize;
 
     // printf("%f \n%f\n", randomCeroUno, random);
-    printf("Memoria Física: %d \nMemoria Virtual: %f\nTamaño de Páginas: %d\n", intMemFisica, memVirtual, intPageSize);
+    printf("Memoria Física: %d MB \nMemoria Virtual: %d MB\nTamaño de Páginas: %d MB\n", intMemFisica, intMemVirtual, intPageSize);
 
     float cantPags = memVirtual / pageSize;
-    printf("Cantidad de Páginas: %f\n", cantPags);
+    int intCantPags = (int)cantPags;
+    printf("Cantidad de Páginas: %d\n", intCantPags);
 
+    int memDisponible = memFisica;
+    float swapDisponible = memVirtual - memFisica;
+
+    bool swap = false, exceso = false;
     int i = 0;
     while(1) {
-        
+        int newPsize = rand() % (RANGO_B - RANGO_A + 1) + RANGO_A;
+        if (memDisponible < newPsize) {
+            if (swapDisponible + memDisponible < newPsize) {
+                printf("Memoria insuficiente en el sistema");
+                return 0;
+            }
+            else {
+                swapDisponible -= (newPsize - memDisponible);
+                memDisponible = 0;
+            }
+        }
+        else {
+            memDisponible -= newPsize;
+        }
+
+        fflush(stdout);
+        i++;
+        sleep(1);
     }
 
     return 0;
